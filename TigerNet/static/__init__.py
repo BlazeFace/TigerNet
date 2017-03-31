6128474c16173dae72bc0c7b6f08b0b2694b0a00
BlazeFace <nate.bensing@gmail.com>
2017-03-31 15:05:32 -0400

    Added Login db

--- TigerNet/static/__init__.py
+++ TigerNet/static/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, template_folder='../templates')
 app.config.from_object('config')
-
-from TigerNet.static import views
+db = SQLAlchemy(app)
+from TigerNet.static import views, models
 
