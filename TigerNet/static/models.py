4ab349f1bf9b94f22a3e607acda072d95e61f54c
BlazeFace <nate.bensing@gmail.com>
2017-04-04 16:47:43 -0400

    Added Login db pt 2

--- TigerNet/static/models.py
+++ TigerNet/static/models.py
@@ -1,7 +1,6 @@
 from TigerNet.static import db
 
-
-class User(db.models):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
